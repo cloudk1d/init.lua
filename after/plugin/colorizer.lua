ec46de4fa25867d3253f30d68ee2f6091986dd91
cloudk1d <jack@nemitz.cloud>
2026-03-19 19:41:47 -0500

    standardize colorizer settings across filetypes

--- after/plugin/colorizer.lua
+++ after/plugin/colorizer.lua
@@ -1,17 +1,11 @@
 require 'colorizer'.setup({
-    javascript = { names = false },
     css = {
-        names = false,
         rgb_fn = true,
-    },
-    xml = {
-        names = false,
-        mode = 'background'
-    },
-    yaml = {
-        mode = 'background'
-    },
-    go = {
-        mode = 'background'
     }
-}, { mode = 'foreground' })
+}, {
+    mode = 'background',
+    names = false,
+    RRGGBBAA = true,
+    css = true,
+    css_fn = true
+})
